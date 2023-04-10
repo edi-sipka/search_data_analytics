fd7b11e057f121d06e790ee4f61d31d833943340
edi-sipka <edisipka@gmail.com>
2023-04-10 16:55:48 +0200

    Added articles seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+articles = Article.create([{ title: "Future Tech" }, { title: "Green Living" } , { title: "New Horizons" } , { title: "Tech Impact" } , { title: "Science meets Health" }, { title: "Digital Wealth" }, { title: "Changing Dynamics" }, { title: "Urban Evolution" }, { title: "Clean Future" }, { title: "Innovation Unleashed" }, { title: "Protecting Data" }, { title: "Ethical Dilemmas" }, { title: "Manufacturing Revolution" }, { title: "Transforming Industries" }, { title: "Wellness Revolution" }, { title: "Digital Realities" }, { title: "Connected World" }, { title: "Protecting Information" }, { title: "Impact of eco savings" }, { title: "Generation of Technology" }, ])
