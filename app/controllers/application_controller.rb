8e905c39fe609fbf82ba49352ee88a250485b4ed
edi-sipka <edisipka@gmail.com>
2023-04-11 14:52:08 +0200

    Functionality to aplication, articles controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
-
+    before_action :set_turbo_frame_request_variant
+  
+    private
+  
+    def set_turbo_frame_request_variant
+      request.variant = :turbo_frame if turbo_frame_request?
+    end
+  
     def logged_user
       @logged_user = User.find_or_create_by(ip_address: request.remote_ip)
     end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,29 @@
 class ArticlesController < ApplicationController
     before_action :logged_user
-
+  
     def index
       if params[:query].present?
+        save_query(params[:query])
         @articles = Article.search_by_title(params[:query]).limit(50)
       else
         @articles = Article.default_limit
       end
+    end
+  
+    private
+  
+    def save_query(query)
+      recent_query = @logged_user.searches.order(created_at: :desc).first
+      return unless should_save_search_query?(query, recent_query)
+  
+      if recent_query.nil? 
+        Search.create(query: query, user: @logged_user)
+      else
+        recent_query.update(query: query)
+      end
+    end
+  
+    def should_save_search_query?(query, recent_query)
+      query.present? && query.length >= 5 && (recent_query.nil? || !recent_query.query.include?(query) || recent_query.query.length < query.length)
     end
   end
