d227b089a8649c30dceea354e88344816b648a8f
edi-sipka <edisipka@gmail.com>
2023-04-11 17:42:14 +0200

    Stlyed navigation and index/article page

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,5 @@ class Article < ApplicationRecord
     validates :title, presence: true, length: { minimum: 1, maximum: 100 }
 
     scope :search_by_title, ->(query) { where('title ILIKE ?', "%#{query}%") }
-    scope :default_limit, -> { limit(20) }
+    scope :default_limit, -> { limit(18) }
   end
