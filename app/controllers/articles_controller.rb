c9747b5ead831a3701f231894eb551b3b382354d
edi-sipka <edisipka@gmail.com>
2023-04-14 16:54:37 +0200

    Add more tests

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,40 +1,45 @@
 class ArticlesController < ApplicationController  
-    def index
-      if query_param.present?
-        save_search if valid_search_request?
-
-        @articles = Article.search_by_title(query_param).limit(50)
-      else
-        @articles = Article.limit(9)
-      end
-    end
   
-    private
-
-    def query_param
-      params[:query]&.strip
-    end
-
-    def latest_search
-      @latest_search ||= Search.in_descending_order_by(current_user).first
-    end
-  
-    def save_search
-      if latest_search.blank? || !related_article(latest_search&.query, query_param)
-        Search.create(query: query_param.strip, user: current_user)
-      else
-        latest_search.update(query: query_param)
-      end
-    end
-  
-    def valid_search_request?
-      return true unless latest_search.present?
-      query_param.length >= 2 && (latest_search.nil? || !latest_search.query.include?(query_param) || latest_search.query.length < query_param.length)
-    end
-
-    def related_article(str1, str2)
-      jarow = FuzzyStringMatch::JaroWinkler.create(:native)
-      distance = jarow.getDistance(str1, str2)
-      distance > 0.8
+  def index
+    if query_param.present?
+      save_search if valid_search_request?
+      @articles = Article.search_by_title(query_param).limit(50)
+    else
+      @articles = Article.limit(9)
     end
   end
+  
+  private
+
+  def query_param
+    params[:query]&.strip
+  end
+
+  def latest_search
+    @latest_search ||= Search.in_descending_order_by(current_user).first
+  end
+  
+  def save_search
+    if should_create_new_search?
+      Search.create(query: query_param.strip, user: current_user)
+    else
+      latest_search.update(query: query_param.strip)
+    end
+  end
+  
+  def should_create_new_search?
+    latest_search.blank? || !related_article(latest_search&.query, query_param)
+  end
+
+  def valid_search_request?
+    return true unless latest_search.present?
+    query_param_length = query_param.to_s.length
+    !latest_search.query.include?(query_param) || latest_search.query.length < query_param_length
+  end
+
+  def related_article(str1, str2)
+    jarow = FuzzyStringMatch::JaroWinkler.create(:native)
+    distance = jarow.getDistance(str1, str2)
+    distance > 0.8
+  end
+end
--- spec/controller/article_spec.rb
+++ spec/controller/article_spec.rb
@@ -19,6 +19,27 @@ RSpec.describe ArticlesController, type: :controller do
         expect(controller).to receive(:save_search)
         get :index
       end
+
+      it 'calls search_by_title on Article with query param' do
+        expect(Article).to receive(:search_by_title).with(query).and_call_original
+        get :index
+      end
+    end
+
+    context 'when query param is not present' do
+      before do
+        allow(controller).to receive(:query_param).and_return(nil)
+      end
+
+      it 'does not call save_search method while query params is nil' do
+        expect(controller).not_to receive(:save_search)
+        get :index
+      end
+
+      it 'limits results to 9' do
+        expect(Article).to receive(:limit).with(9).and_call_original
+        get :index
+      end
     end
 
     context 'when valid_search_request? is false' do
@@ -33,4 +54,31 @@ RSpec.describe ArticlesController, type: :controller do
       end
     end
   end
-end
+  
+  describe 'should_create_new_search?' do
+    subject { controller.send(:should_create_new_search?) }
+  
+    context 'when latest_search is blank' do
+      before do
+        allow(controller).to receive(:latest_search).and_return(nil)
+      end
+  
+      it { is_expected.to be_truthy }
+    end
+  end
+
+  describe 'should_create_new_search?' do
+    subject { controller.send(:should_create_new_search?) }
+  
+    context 'when latest_search is present and related article found' do
+      let(:latest_search) { double(query: 'related query') }
+  
+      before do
+        allow(controller).to receive(:latest_search).and_return(latest_search)
+        allow(controller).to receive(:related_article).and_return(true)
+      end
+  
+      it { is_expected.to be_falsy }
+    end
+  end
+ end
